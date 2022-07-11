908d4667fb3afd90b5a5a4032049c9f0c6268e0c
Filipe Liu <fliu@anl.gov>
2022-07-11 02:36:03 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='modelseedpy_ext',
+      version='0.0.9',
+      description='ModelSEEDpy External Tools',
+      url='https://github.com/Fxe/ModelSEEDpy-Ext',
+      author='Filipe Liu',
+      author_email='fliu@anl.gov',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          # "modelseedpy >= 1.0.0", # when available in pypi
+          "pandas >= 1.0.0",
+          "networkx >= 2.4",
+          "cobra >= 0.17.1"
+      ],
+      zip_safe=True)
