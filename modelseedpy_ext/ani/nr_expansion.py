5bbcfbd9440700883d2734ea5155448b0686a07a
Filipe Liu <fliu@anl.gov>
2024-10-13 23:40:26 -0500

    exp

--- modelseedpy_ext/ani/nr_expansion.py
+++ modelseedpy_ext/ani/nr_expansion.py
@@ -35,7 +35,7 @@ class ExpMethod:
         rep = self._get_best_quality(candidates)
 
         # setup singleton cluster
-        self.clusters[rep] = {rep}
+        self.clusters[rep][rep] = 100  # self ani assume 100
 
         # get entities in the candidate pool that has ANI range of ani_radius
         for o in self.all_vs_all.filter(
@@ -43,9 +43,9 @@ class ExpMethod:
                 (pl.col("Query_file2") == rep)
         ).rows(named=True):
             if o['Query_file1'] == rep:
-                clusters[rep].add(o['Query_file2'])
+                clusters[rep][o['Query_file2']] = o['ANI']
             else:
-                clusters[rep].add(o['Query_file1'])
+                clusters[rep][o['Query_file1']] = o['ANI']
 
         visited |= set(self.clusters[rep])
         return candidates - visited
@@ -53,45 +53,84 @@ class ExpMethod:
 
 class NrExp:
 
-    def __init__(self):
+    MOCK_PRE_COMP_DATA = {
+        'fastgenomics': (
+            '/scratch/fliu/data/ani/library_morgan.txt',
+            '/scratch/fliu/data/ani/morgan_vs_gtdb_r220_rep.out',
+            '/scratch/fliu/data/ani/skani/morgan.triangle.slow.out'),
+        'alexey': (
+            '/scratch/fliu/data/ani/library_alexey.txt',
+            '/scratch/fliu/data/ani/alexey_vs_gtdb_r220_rep.out',
+            '/scratch/fliu/data/ani/skani/alexey.triangle.slow.out'),
+        'grow': (
+            '/scratch/fliu/data/ani/library_grow_kbase.txt',
+            '/scratch/fliu/data/ani/skani/grow_vs_gtdb_r220_rep.slow.out',
+            '/scratch/fliu/data/ani/grow_kbase_vs_self.out'),
+        'mucc': (
+            '/scratch/fliu/data/ani/library_mucc_kbase.txt',
+            '/scratch/fliu/data/ani/mucc_kbase_vs_gtdb_r220_rep.out',
+            '/scratch/fliu/data/ani/mucc_kbase_vs_self.out'),
+        'anme': (
+            '/scratch/fliu/data/ani/library_anme_kbase.txt',
+            '/scratch/fliu/data/ani/skani/anme_vs_gtdb_r220_rep.slow.out',
+            '/scratch/fliu/data/ani/skani/anme.triangle.slow.out'),
+    }
+
+    def __init__(self, ani_radius=95):
         self.path_to_exp_library = '/scratch/fliu/data/ani/skani/exp'
         self.rep_exp_counter = 0
         self.rep_clusters = {}
+        self.member_to_cluster = {}
         self.dao_ani = None
         self.ani_method = None
         self.rep_exp_libraries = {}
-        self.ani_radius = 95
+        self.ani_radius = ani_radius
 
-    def expand(self, exp_id, genome_files, file_vs_rep, file_vs_self):
+    def run_ani(self, query_library, rep_library, genome_files):
 
-        df_vs_rep = pl.from_arrow(_read_search_output_as_parquet(file_vs_rep, sep='\t'))
-        print('number of ani pairs:', len(df_vs_rep))
+        if rep_library == 'gtdb_r220_rep' and query_library in self.MOCK_PRE_COMP_DATA:
+            file_vs_rep = self.MOCK_PRE_COMP_DATA[query_library][1]
+            return pl.from_arrow(_read_search_output_as_parquet(file_vs_rep, sep='\t'))
+        elif query_library == rep_library and query_library in self.MOCK_PRE_COMP_DATA:
+            file_vs_self = self.MOCK_PRE_COMP_DATA[query_library][2]
+            return pl.from_arrow(_read_triangle_output_as_parquet(file_vs_self, sep='\t'))
+        else:
+            self.ani_method.distance(genome_files,
+                                     rep_library[:-11],
+                                     f'{self.path_to_exp_library}/{query_library}_{rep_library}.out')
+        return None
 
-        to_previous_representatives = df_vs_rep.filter(
-            pl.col("ANI") >= self.ani_radius).group_by(
-            "Query_file", maintain_order=True).max()
+    def expand(self, query_library, genome_files):
 
         added = set()
-        for o in to_previous_representatives.rows(named=True):
-            rep = o['Ref_file'].split('/')[-1]
-            genome_id = o['Query_file'].split('/')[-1]
-            if rep not in self.rep_clusters:
-                self.rep_clusters[rep] = {}
-            self.rep_clusters[rep][genome_id] = o['ANI']
-            added.add(o['Query_file'])
-        print('members added to rep clusters:', len(added))
+        candidates = set()
 
-        for exp_iteration, exp_library in self.rep_exp_libraries.items():
-            self.ani_method.distance(genome_files,
-                                     exp_library[:-11],
-                                     f'{self.path_to_exp_library}/{exp_id}_iteration{exp_iteration}.out')
+        for exp_library in self.rep_exp_libraries:
             print(exp_library)
+            if exp_library == 'gtdb_r220_rep':
+                df_vs_rep = self.run_ani(query_library, exp_library, genome_files)
+                print(f'number of ani pairs ({exp_library}):', len(df_vs_rep))
 
-        candidates_for_representatives = df_vs_rep.filter(~pl.col("Query_file").is_in(added))
-        candidates = set(candidates_for_representatives['Query_file'])
-        print('members candidate for new rep clusters:', len(candidates))
+                # select all members with ANI >= radius
+                to_previous_representatives = df_vs_rep.filter(
+                    pl.col("ANI") >= self.ani_radius).group_by(
+                    "Query_file", maintain_order=True).max()
 
-        df_all_vs_all = pl.from_arrow(_read_triangle_output_as_parquet(file_vs_self, sep='\t'))
+                for o in to_previous_representatives.rows(named=True):
+                    rep = o['Ref_file']
+                    genome_id = o['Query_file']
+                    if genome_id not in self.member_to_cluster:
+
+                        if rep not in self.rep_clusters:
+                            self.rep_clusters[rep] = {}
+                        self.rep_clusters[rep][genome_id] = o['ANI']
+                        self.member_to_cluster[genome_id] = rep
+                        added.add(o['Query_file'])
+                print('members added to rep clusters:', len(added))
+                candidates -= added
+                print('members candidate to new rep clusters:', len(candidates))
+            
+        df_all_vs_all = self.run_ani(query_library, query_library, genome_files)
         df_all_vs_all = df_all_vs_all.filter(pl.col(
             "Query_file1").is_in(candidates) & pl.col(
             "Query_file2").is_in(candidates)).filter(pl.col("ANI") >= self.ani_radius)
@@ -101,15 +140,23 @@ class NrExp:
 
         print('new clusters', len(exp_method.clusters))
 
-        self.catalog_new_representatives(exp_method)
+        self.catalog_new_representatives(exp_method, query_library)
 
         self.rep_exp_counter += 1
 
-    def catalog_new_representatives(self, exp_method):
+    def catalog_new_representatives(self, exp_method, query_library):
         if len(exp_method.clusters) > 0:
             genome_files = [GenomeReference.from_file(x) for x in exp_method.clusters.keys()]
             exp_library_file = f'{self.path_to_exp_library}/iteration_{self.rep_exp_counter}.txt'
             ani_library = self.ani_method.build_library(genome_files, exp_library_file)
-            self.rep_exp_libraries[self.rep_exp_counter] = ani_library
+            self.rep_exp_libraries[query_library] = ani_library
 
-            self.rep_clusters.update(exp_method.clusters)
+            for k, members in exp_method.clusters.items():
+                if k not in self.member_to_cluster:
+                    if k not in self.rep_clusters:
+                        self.rep_clusters[k] = {}
+                        for m in members:
+                            if m not in self.member_to_cluster:
+                                self.member_to_cluster[m] = k
+                                self.rep_clusters[k][m] = members[m]
+            #self.rep_clusters.update(exp_method.clusters)
